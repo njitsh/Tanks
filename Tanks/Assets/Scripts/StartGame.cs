1eda85149ef2a081b8147170639faa8cf1b13f19
Maurice <Mauricederidder@outlook.com>
2019-06-25 18:26:43 +0200

    Scene Management Changes
    Added start scene.
    Added Button to open Main Game
    Added Editor Scene and button

---
+++ Tanks/Assets/Scripts/StartGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System;
+
+public class StartGame : MonoBehaviour
+{
+
+    public Button StartButton;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (StartButton != null)
+        {
+            StartButton.onClick.AddListener(LoadMainScene);
+        }
+        
+    }
+    void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainGameScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
