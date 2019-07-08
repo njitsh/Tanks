9230c383dd89f406e5c0a97acd1ee766a64529da
MrRedstoneWolf <stijn@sharks.nl>
2019-07-08 23:32:05 +0200

    Add Menu + Pause and Multiple Tanks

---
+++ Tanks/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void LocalGame()
+    {
+        SceneManager.LoadScene("LocalGameScene");
+        Cursor.visible = false;
+    }
+
+    public void OnlineGame()
+    {
+        SceneManager.LoadScene("OnlineGameScene");
+    }
+
+    public void MapEditor()
+    {
+        SceneManager.LoadScene("MapEditorScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Tanks/Assets/Scripts/PlayerGun.cs
+++ Tanks/Assets/Scripts/PlayerGun.cs
@@ -7,21 +7,25 @@ public class PlayerGun : MonoBehaviour
     private Rigidbody2D rb;
     private Vector3 moveVelocity;
     private float target_angle, angle;
+    public GameObject parentTank;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        transform.position = GameObject.Find("Player").transform.position;
+        transform.position = parentTank.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = GameObject.Find("Player").transform.position;
-        var pos = Camera.main.WorldToScreenPoint(transform.position);
-        var dir = Input.mousePosition - pos;
-        angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 360) % 360;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (!PauseMenu.GameIsPaused)
+        {
+            transform.position = parentTank.transform.position;
+            var pos = Camera.main.WorldToScreenPoint(transform.position);
+            var dir = Input.mousePosition - pos;
+            angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 360) % 360;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
