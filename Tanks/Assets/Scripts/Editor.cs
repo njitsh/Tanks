8ba172080a5183db1755c52f14e9e7b51c0548f6
MrRedstoneWolf <stijn@sharks.nl>
2019-10-15 17:25:31 +0200

    Add Save and Load function + Clear function + Set camera position to mid

--- Tanks/Assets/Scripts/Editor.cs
+++ Tanks/Assets/Scripts/Editor.cs
@@ -7,24 +7,33 @@ public class Editor : MonoBehaviour
 {
     public Tile selectedTile = null;
 
-    public Tilemap tilemap;
-    public Tilemap selectedTilemap; // Temp place tiles on this layer to show the selected block and location
+    public Tilemap tilemapGround;
+    public Tilemap selectedTilemapGround; // Temp place tiles on this layer to show the selected block and location
 
     Vector3Int currentCell;
     Vector3Int previousCell;
 
-    bool eraseMode = false;
+    bool eraseMode;
+
+    public readonly int maxWidth = 30;
+    public readonly int maxHeight = 20;
+
+    private void Awake()
+    {
+        SaveSystem.Init();
+        GameObject.Find("Main Camera").transform.position = tilemapGround.CellToWorld(new Vector3Int(maxWidth/2, maxHeight/2, 0));
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (!PauseMenu.GameIsPaused)
         {
-            currentCell = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+            currentCell = tilemapGround.WorldToCell(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
             if (selectedTile != null && currentCell != previousCell)
             {
-                selectedTilemap.SetTile(currentCell, selectedTile);
-                selectedTilemap.SetTile(previousCell, null);
+                selectedTilemapGround.SetTile(currentCell, selectedTile);
+                selectedTilemapGround.SetTile(previousCell, null);
                 previousCell = currentCell;
             };
 
@@ -33,14 +42,15 @@ public class Editor : MonoBehaviour
             //else Cursor.visible = true;
             if (Input.GetKey(KeyCode.Mouse0))
             {
-                if (!eraseMode && selectedTile != null)
+                Debug.Log(maxHeight + " " + maxWidth);
+                if (!eraseMode && selectedTile != null && currentCell.x >= 0 && currentCell.x < maxWidth && currentCell.y >= 0 && currentCell.y < maxHeight)
                 {
-                    tilemap.SetTile(currentCell, selectedTile);
-                    selectedTilemap.SetTile(currentCell, null);
+                    tilemapGround.SetTile(currentCell, selectedTile);
+                    selectedTilemapGround.SetTile(currentCell, null);
                 }
                 else if (eraseMode)
                 {
-                    tilemap.SetTile(currentCell, null);
+                    tilemapGround.SetTile(currentCell, null);
                 }
                 else
                 {
@@ -50,13 +60,13 @@ public class Editor : MonoBehaviour
             else if ((Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Mouse1)) && selectedTile != null)
             {
                 selectedTile = null;
-                selectedTilemap.SetTile(currentCell, null);
+                selectedTilemapGround.SetTile(currentCell, null);
                 eraseMode = false;
             }
             else if (Input.GetKeyDown(KeyCode.X))
             {
                 selectedTile = null;
-                selectedTilemap.SetTile(currentCell, null);
+                selectedTilemapGround.SetTile(currentCell, null);
                 eraseMode = !eraseMode;
             }
         }
@@ -65,5 +75,76 @@ public class Editor : MonoBehaviour
     public void Select_Tile(Tile Selected_Tile)
     {
         selectedTile = Selected_Tile;
+        eraseMode = false;
+    }
+
+    public void Save_Map()
+    {
+        // Get bounds
+        BoundsInt bounds = tilemapGround.cellBounds;
+        int bounds_ground_x = bounds.size.x;
+        int bounds_ground_y = bounds.size.y;
+
+        TileBase[] allTiles = tilemapGround.GetTilesBlock(bounds);
+
+        SaveObject saveObject = new SaveObject
+        {
+            bounds_ground_x = bounds_ground_x,
+            bounds_ground_y = bounds_ground_y,
+            tile_list = new List<TileBase>(allTiles)
+        };
+
+        string json = JsonUtility.ToJson(saveObject);
+        SaveSystem.Save(json);
+
+        Debug.Log("Saved!");
+    }
+
+    public void Load_Map()
+    {
+        string mapString = SaveSystem.Load();
+        if (mapString != null)
+        {
+            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(mapString);
+
+            Open_Map(saveObject.tile_list.ToArray(), saveObject.bounds_ground_x, saveObject.bounds_ground_y);
+
+            Debug.Log("Loaded!");
+        }
+        else
+        {
+            Debug.Log("No map was loaded!");
+        }
+    }
+
+    private void Open_Map(TileBase[] map_ground, int bounds_x, int bounds_y)
+    {
+        for (int x = 0; x < bounds_x; x++)
+        {
+            for (int y = 0; y < bounds_y; y++)
+            {
+                tilemapGround.SetTile(new Vector3Int(x, y, 0), map_ground[x + y * bounds_x]);
+            }
+        }
+    }
+
+    public void Clear_Map()
+    {
+        BoundsInt bounds = tilemapGround.cellBounds;
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                tilemapGround.SetTile(new Vector3Int(x, y, 0), null);
+            }
+        }
+    }
+
+    public class SaveObject
+    {
+        public int bounds_ground_x;
+        public int bounds_ground_y;
+        public List<TileBase> tile_list;
     }
 }
---
+++ Tanks/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static readonly string MAP_FOLDER = Application.dataPath + "/Maps/";
+
+    // Start is called before the first frame update
+    public static void Init()
+    {
+        // Test if map folder exists
+        if (!Directory.Exists(MAP_FOLDER))
+        {
+            // Create map folder
+            Directory.CreateDirectory(MAP_FOLDER);
+        }
+    }
+
+    // SAVE MAP
+    public static void Save(string mapString)
+    {
+        int mapNumber = 1;
+        while (File.Exists(MAP_FOLDER + "map_" + mapNumber + ".json"))
+        {
+            mapNumber++;
+        }
+        File.WriteAllText(MAP_FOLDER + "map_" + mapNumber + ".json", mapString);
+    }
+
+    // LOAD MAP
+    public static string Load()
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(MAP_FOLDER);
+        FileInfo[] mapFiles = directoryInfo.GetFiles("*.json");
+        FileInfo mostRecentFile = null;
+        foreach (FileInfo fileInfo in mapFiles)
+        {
+            if (mostRecentFile == null)
+            {
+                mostRecentFile = fileInfo;
+            }
+            else
+            {
+                if (fileInfo.LastWriteTime > mostRecentFile.LastWriteTime)
+                {
+                    mostRecentFile = fileInfo;
+                }
+            }
+        }
+
+        if (mostRecentFile != null)
+        {
+            string mapString = File.ReadAllText(mostRecentFile.FullName);
+            return mapString;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
