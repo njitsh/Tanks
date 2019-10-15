39c0fc6aed96b4d8c2dc9fb13e6de16b0075cc5f
MrRedstoneWolf <stijn@sharks.nl>
2019-10-16 00:19:50 +0200

    Add Objects layer

--- Tanks/Assets/Scripts/Editor.cs
+++ Tanks/Assets/Scripts/Editor.cs
@@ -8,10 +8,13 @@ public class Editor : MonoBehaviour
     public Tile selectedTile = null;
 
     public Tilemap tilemapGround;
-    public Tilemap selectedTilemapGround; // Temp place tiles on this layer to show the selected block and location
+    public Tilemap tilemapObjects;
 
     Vector3Int currentCell;
     Vector3Int previousCell;
+
+    Tilemap activeMap;
+    Tilemap activeSelectedMap;
 
     bool eraseMode;
 
@@ -21,7 +24,7 @@ public class Editor : MonoBehaviour
     private void Awake()
     {
         SaveSystem.Init();
-        GameObject.Find("Main Camera").transform.position = tilemapGround.CellToWorld(new Vector3Int(maxWidth/2, maxHeight/2, 0));
+        GameObject.Find("Main Camera").transform.position = tilemapGround.CellToWorld(new Vector3Int(maxWidth/2, maxHeight/2, 0)) + new Vector3Int(0,0,-10);
     }
 
     // Update is called once per frame
@@ -29,45 +32,44 @@ public class Editor : MonoBehaviour
     {
         if (!PauseMenu.GameIsPaused)
         {
-            currentCell = tilemapGround.WorldToCell(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
-            if (selectedTile != null && currentCell != previousCell)
+            if (activeMap != null && activeSelectedMap != null)
             {
-                selectedTilemapGround.SetTile(currentCell, selectedTile);
-                selectedTilemapGround.SetTile(previousCell, null);
-                previousCell = currentCell;
-            };
+                currentCell = activeSelectedMap.WorldToCell(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+                if (selectedTile != null && currentCell != previousCell)
+                {
+                    activeSelectedMap.SetTile(currentCell, selectedTile);
+                    activeSelectedMap.SetTile(previousCell, null);
+                    previousCell = currentCell;
+                }
 
-
-            //if (selectedTile != null) Cursor.visible = false;
-            //else Cursor.visible = true;
-            if (Input.GetKey(KeyCode.Mouse0))
-            {
-                Debug.Log(maxHeight + " " + maxWidth);
-                if (!eraseMode && selectedTile != null && currentCell.x >= 0 && currentCell.x < maxWidth && currentCell.y >= 0 && currentCell.y < maxHeight)
+                if (Input.GetKey(KeyCode.Mouse0))
                 {
-                    tilemapGround.SetTile(currentCell, selectedTile);
-                    selectedTilemapGround.SetTile(currentCell, null);
+                    if (!eraseMode && selectedTile != null && ((currentCell.x >= 0 && currentCell.x < maxWidth && currentCell.y >= 0 && currentCell.y < maxHeight && activeMap == tilemapGround) || (currentCell.x >= 0 && currentCell.x < maxWidth * 2 && currentCell.y >= 0 && currentCell.y < maxHeight * 2 && activeMap == tilemapObjects)))
+                    {
+                        activeMap.SetTile(currentCell, selectedTile);
+                        activeSelectedMap.SetTile(currentCell, null);
+                    }
+                    else if (eraseMode)
+                    {
+                        activeMap.SetTile(currentCell, null);
+                    }
+                    else
+                    {
+                        // Maybe add function to select blocks
+                    }
                 }
-                else if (eraseMode)
+                else if ((Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Mouse1)) && selectedTile != null)
                 {
-                    tilemapGround.SetTile(currentCell, null);
+                    selectedTile = null;
+                    activeSelectedMap.SetTile(currentCell, null);
+                    eraseMode = false;
                 }
-                else
+                else if (Input.GetKeyDown(KeyCode.X))
                 {
-                    // Maybe add function to select blocks
+                    selectedTile = null;
+                    activeSelectedMap.SetTile(currentCell, null);
+                    eraseMode = !eraseMode;
                 }
-            }
-            else if ((Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Mouse1)) && selectedTile != null)
-            {
-                selectedTile = null;
-                selectedTilemapGround.SetTile(currentCell, null);
-                eraseMode = false;
-            }
-            else if (Input.GetKeyDown(KeyCode.X))
-            {
-                selectedTile = null;
-                selectedTilemapGround.SetTile(currentCell, null);
-                eraseMode = !eraseMode;
             }
         }
     }
@@ -78,26 +80,43 @@ public class Editor : MonoBehaviour
         eraseMode = false;
     }
 
+    public void ActiveMap(Tilemap active_map)
+    {
+        activeMap = active_map;
+    }
+
+    public void ActiveSelectedMap(Tilemap active_selected_map)
+    {
+        activeSelectedMap = active_selected_map;
+    }
+
     public void Save_Map()
     {
         // Get bounds
-        BoundsInt bounds = tilemapGround.cellBounds;
-        int bounds_ground_x = bounds.size.x;
-        int bounds_ground_y = bounds.size.y;
+        BoundsInt bounds_ground = tilemapGround.cellBounds;
+        int bounds_ground_x = bounds_ground.size.x;
+        int bounds_ground_y = bounds_ground.size.y;
 
-        TileBase[] allTiles = tilemapGround.GetTilesBlock(bounds);
+        TileBase[] allTiles_ground = tilemapGround.GetTilesBlock(bounds_ground);
+
+        BoundsInt bounds_objects = tilemapObjects.cellBounds;
+        int bounds_objects_x = bounds_objects.size.x;
+        int bounds_objects_y = bounds_objects.size.y;
+
+        TileBase[] allTiles_objects = tilemapObjects.GetTilesBlock(bounds_objects);
 
         SaveObject saveObject = new SaveObject
         {
             bounds_ground_x = bounds_ground_x,
             bounds_ground_y = bounds_ground_y,
-            tile_list = new List<TileBase>(allTiles)
+            tile_list_ground = new List<TileBase>(allTiles_ground),
+            bounds_objects_x = bounds_objects_x,
+            bounds_objects_y = bounds_objects_y,
+            tile_list_objects = new List<TileBase>(allTiles_objects)
         };
 
         string json = JsonUtility.ToJson(saveObject);
         SaveSystem.Save(json);
-
-        Debug.Log("Saved!");
     }
 
     public void Load_Map()
@@ -107,9 +126,7 @@ public class Editor : MonoBehaviour
         {
             SaveObject saveObject = JsonUtility.FromJson<SaveObject>(mapString);
 
-            Open_Map(saveObject.tile_list.ToArray(), saveObject.bounds_ground_x, saveObject.bounds_ground_y);
-
-            Debug.Log("Loaded!");
+            Open_Map(saveObject.tile_list_ground.ToArray(), saveObject.bounds_ground_x, saveObject.bounds_ground_y, saveObject.tile_list_objects.ToArray(), saveObject.bounds_objects_x, saveObject.bounds_objects_y);
         }
         else
         {
@@ -117,26 +134,41 @@ public class Editor : MonoBehaviour
         }
     }
 
-    private void Open_Map(TileBase[] map_ground, int bounds_x, int bounds_y)
+    private void Open_Map(TileBase[] map_ground, int bounds_g_x, int bounds_g_y, TileBase[] map_objects, int bounds_o_x, int bounds_o_y)
     {
-        for (int x = 0; x < bounds_x; x++)
+        for (int x = 0; x < bounds_g_x; x++)
         {
-            for (int y = 0; y < bounds_y; y++)
+            for (int y = 0; y < bounds_g_y; y++)
             {
-                tilemapGround.SetTile(new Vector3Int(x, y, 0), map_ground[x + y * bounds_x]);
+                tilemapGround.SetTile(new Vector3Int(x, y, 0), map_ground[x + y * bounds_g_x]);
+            }
+        }
+        for (int x = 0; x < bounds_o_x; x++)
+        {
+            for (int y = 0; y < bounds_o_y; y++)
+            {
+                tilemapObjects.SetTile(new Vector3Int(x, y, 0), map_objects[x + y * bounds_o_x]);
             }
         }
     }
 
     public void Clear_Map()
     {
-        BoundsInt bounds = tilemapGround.cellBounds;
-
-        for (int x = 0; x < bounds.size.x; x++)
+        BoundsInt bounds_ground = tilemapGround.cellBounds;
+        for (int x = 0; x < bounds_ground.size.x; x++)
         {
-            for (int y = 0; y < bounds.size.y; y++)
+            for (int y = 0; y < bounds_ground.size.y; y++)
             {
                 tilemapGround.SetTile(new Vector3Int(x, y, 0), null);
+            }
+        }
+
+        BoundsInt bounds_objects = tilemapGround.cellBounds;
+        for (int x = 0; x < bounds_objects.size.x; x++)
+        {
+            for (int y = 0; y < bounds_objects.size.y; y++)
+            {
+                tilemapObjects.SetTile(new Vector3Int(x, y, 0), null);
             }
         }
     }
@@ -145,6 +177,9 @@ public class Editor : MonoBehaviour
     {
         public int bounds_ground_x;
         public int bounds_ground_y;
-        public List<TileBase> tile_list;
+        public List<TileBase> tile_list_ground;
+        public int bounds_objects_x;
+        public int bounds_objects_y;
+        public List<TileBase> tile_list_objects;
     }
 }
