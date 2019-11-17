a980b047b8fd7b19bfed5c4ab4065a9bb34d03cc
Stijn <stijn@sharks.nl>
2019-11-17 14:26:19 +0100

    Update Unity to 2019.2.12f1

--- Tanks/Assets/Scripts/LevelSelector.cs
+++ Tanks/Assets/Scripts/LevelSelector.cs
@@ -13,26 +13,33 @@ public class LevelSelector : MonoBehaviour
     public string level_path;
 
     int blockSpacing = 110;
+    int i;
+
     // int maxFileNameLength = 10; TODO ADD MAX FILENAME LENGTH
 
     void Start()
     {
-        FileInfo[] mapFiles = SaveSystem.GetSavesFromFolder(0);
-        int i = 0;
-        foreach (FileInfo mapFile in mapFiles)
-        {
-            LevelBlockList.Add(Instantiate(Level_Block, transform));
-            string fileName = Path.GetFileNameWithoutExtension(mapFile.ToString());
-            LevelBlockList[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(fileName);
-            LevelBlockList[i].transform.position -= new Vector3(0, blockSpacing * i, 0);
-            LevelBlockList[i].GetComponent<Button>().onClick.AddListener(() => setSelectedLevelPath(mapFile.ToString()));
-            i++;
-        }
+        listLevels(SaveSystem.GetSavesFromFolder(0));
+        listLevels(SaveSystem.GetSavesFromFolder(1));
     }
 
     void setSelectedLevelPath(string filepath)
     {
         level_path = filepath;
         GameObject.Find("LobbyCanvas").GetComponent<LobbyMenu>().StartLocalGame();
+    }
+
+    void listLevels(FileInfo[] mapFiles)
+    {
+        foreach (FileInfo mapFile in mapFiles)
+        {
+            LevelBlockList.Add(Instantiate(Level_Block, transform.GetChild(0).GetChild(0)));
+            string fileName = Path.GetFileNameWithoutExtension(mapFile.ToString());
+            LevelBlockList[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().SetText(fileName);
+            LevelBlockList[i].transform.position -= new Vector3(0, blockSpacing * i + 60, 0);
+            transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (blockSpacing + 20) * i + 10);
+            LevelBlockList[i].GetComponent<Button>().onClick.AddListener(() => setSelectedLevelPath(mapFile.ToString()));
+            i++;
+        }
     }
 }
